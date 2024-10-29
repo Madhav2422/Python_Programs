7dc647abe6db85d0efda7ebdf0a1d517331bd1a0
Madhav Hirani <madhavhirani2422@gmail.com>
2024-10-29 20:01:01 +0530

    Strings

---
+++ p4strings.py
@@ -0,0 +1,27 @@
+text='ice cream'
+
+# String slicing
+print(text)
+print(text[0])
+
+# Strings are immutable means you cannot change it
+# text[0]=h you cannot do like this
+
+ # Print Substring
+print(text[0:3])
+print(text[4:])
+print(text[:3])
+
+
+                # Print in multiple lines
+propose='''i love you 
+Will you marry me '''
+print(propose)
+
+s=2
+name='maddy'
+
+# print(name+s)
+
+        # Convert num into string
+print(str(s)+name)
