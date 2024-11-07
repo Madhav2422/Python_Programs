b10d5d8fef1b0020bce6e28c554fec264c0ac270
Madhav Hirani <madhavhirani2422@gmail.com>
2024-11-07 13:36:34 +0530

    Documentation Strings

---
+++ p19documentation_strings.py
@@ -0,0 +1,16 @@
+def sub(a,b=8):
+
+    '''
+   This function takes two arguments which are integer numbers  and
+   it will return sum of them as a output
+    '''
+
+    print("a:",a)
+    print("b:",b)
+
+    sub=a-b
+    return sub
+
+# Named Arguments
+n=sub(10)
+print("Total",n)
