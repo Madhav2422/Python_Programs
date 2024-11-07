72624c86ad446611daabafc9f9cc63b6008543a3
Madhav Hirani <madhavhirani2422@gmail.com>
2024-11-07 13:37:13 +0530

    Tuples

---
+++ p21tuples.py
@@ -0,0 +1,12 @@
+# Tuples list of values grouped togther
+# Difference between lists and tuples
+# 1:Lists:All values have similar meaning(Homogeneous)
+#  Tuples:All values have different meaning(Heterogeneous)
+#  2:Lists are mutable(we can change)
+#    Tuples are immutable(we cannot change)
+
+point=(3,5)
+print(point)
+
+# Accessing elements
+print(point[0])
