6c5a281154ade33e31f91fc474d790079214f2bb
Madhav Hirani <madhavhirani2422@gmail.com>
2024-11-10 01:21:07 +0530

    Table

---
+++ p30table.py
@@ -0,0 +1,16 @@
+# n=int(input("Enter the number "))
+#
+# for i in range(1,11):
+#     result=n*i
+#     print(f"{n} X {i} ={result}")
+#
+
+
+# While loop
+n=int(input("Enter the number "))
+
+i=1
+while i<=10:
+    result=n*i
+    print(f"{n} X {i} ={result}")
+    i=i+1
