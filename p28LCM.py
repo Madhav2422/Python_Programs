b8e327cab15d2d96f19083d018eab0894f1c1ae9
Madhav Hirani <madhavhirani2422@gmail.com>
2024-11-10 01:01:17 +0530

    LCM

---
+++ p28LCM.py
@@ -0,0 +1,12 @@
+print("Enter two numbers")
+a=int(input("Enter first number"))
+b=int(input("Enter second number"))
+
+L=a if a>b else b
+
+while L<=a*b:
+    if L%a==0 and L%b==0:
+        print("LCM is ",L)
+        break;
+    L=L+1
+print()
