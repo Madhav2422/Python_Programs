c5b125494f404454306f4fa8ffc13dcd143c31aa
Madhav Hirani <madhavhirani2422@gmail.com>
2024-11-12 18:47:14 +0530

    Weather Activity

---
+++ p35weather_activity.py
@@ -0,0 +1,10 @@
+weather=(input("Enter your weather details"))
+
+if weather=="Sunny":
+    print("Go for a walk")
+elif weather=="Rainy":
+    print("Read a book")
+elif weather=="Snowy":
+    print("Build a snowman")
+else:
+    print("Enter correct weather details")
